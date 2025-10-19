c40092d36e0d1be22119e011516c1a0bb03597db
kayigmb <kayigm1@gmail.com>
2025-10-19 22:29:45 +0200

    malloc

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  *init_dog - initializes a variable of type struct dog.
+  *
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
